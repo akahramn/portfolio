c15291cc9bd9f346d9ecf18d9a8fb6f1e0a40054
Abdullah Kahraman <a.kahramnn@gmail.com>
2024-07-15 11:41:41 +0300

    adding profile photo

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import myImg from "../../Assets/avatar.svg";
+import profilePhoto from "../../Assets/img.png";
 import Tilt from "react-parallax-tilt";
 import {
   AiFillGithub,
@@ -35,7 +36,7 @@ function Home2() {
           </Col>
           <Col md={4} className="myAvtar">
             <Tilt>
-              <img src={myImg} className="img-fluid" alt="avatar" />
+              <img style={{borderRadius: "50%"}} src={profilePhoto} className="img-fluid" alt="avatar" />
             </Tilt>
           </Col>
         </Row>
